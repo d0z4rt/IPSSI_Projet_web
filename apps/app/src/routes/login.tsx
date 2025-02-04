2eaf3cf4626af92c6fcd2d2737713e200899cc6b
d0z4rt <d0z@d0z.eu>
2025-02-04 16:51:55 +0100

    feat: add login page

---
+++ apps/app/src/routes/login.tsx
@@ -0,0 +1,134 @@
+import { JSX, Show, createSignal } from 'solid-js'
+import { createStore } from 'solid-js/store'
+import FormInput from '../components/FormInput'
+
+const Login = () => {
+  const [error, setError] = createSignal('')
+  const [isLoading, setIsLoading] = createSignal(false)
+  const [isRegister, setIsRegister] = createSignal(false)
+  const [loginFields, setLoginFields] = createStore({ email: '', password: '' })
+  const [registerFields, setRegisterFields] = createStore({
+    name: '',
+    email: '',
+    password: '',
+    'repeat-password': ''
+  })
+
+  const [registerName, setName] = createSignal('')
+
+  const toggleRegister = () => {
+    setIsRegister((r) => !r)
+  }
+
+  const handleSubmit: JSX.EventHandler<HTMLFormElement, SubmitEvent> = async (
+    e
+  ) => {
+    e.preventDefault()
+    setIsLoading(true)
+    setError('')
+    try {
+      if (isRegister()) {
+        const res = await fetch('http://127.0.0.1:4000/auth/register', {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(registerFields)
+        })
+        const resBody = await res.json()
+        if (!res.ok) {
+          throw resBody
+        }
+        toggleRegister()
+      } else {
+        const res = await fetch('http://127.0.0.1:4000/auth/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(loginFields)
+        })
+        const resBody = await res.json()
+        if (!res.ok) {
+          throw resBody
+        }
+      }
+    } catch (error) {
+      // ! Yeah ugly stuff...
+      setError((error as { message: string }).message)
+    }
+    setIsLoading(false)
+  }
+
+  return (
+    <main>
+      <Show
+        when={isRegister()}
+        fallback={
+          <>
+            <form onSubmit={handleSubmit}>
+              <h1>Login</h1>
+              <FormInput
+                name="email"
+                disabled={isLoading()}
+                onInput={(e) => setLoginFields('email', e.currentTarget.value)}
+              />
+              <FormInput
+                name="password"
+                disabled={isLoading()}
+                onInput={(e) =>
+                  setLoginFields('password', e.currentTarget.value)
+                }
+              />
+              <button type="submit">login</button>
+            </form>
+            <p>
+              Pas de compte ?
+              <button type="button" onClick={toggleRegister}>
+                Register
+              </button>
+            </p>
+          </>
+        }
+      >
+        <h1>Register</h1>
+        <form onSubmit={handleSubmit}>
+          <FormInput
+            name="name"
+            disabled={isLoading()}
+            onInput={(e) => setRegisterFields('name', e.currentTarget.value)}
+          />
+          <FormInput
+            name="email"
+            disabled={isLoading()}
+            onInput={(e) => setRegisterFields('email', e.currentTarget.value)}
+          />
+          <FormInput
+            name="password"
+            disabled={isLoading()}
+            onInput={(e) =>
+              setRegisterFields('password', e.currentTarget.value)
+            }
+          />
+          <FormInput
+            name="repeat-password"
+            disabled={isLoading()}
+            onInput={(e) =>
+              setRegisterFields('repeat-password', e.currentTarget.value)
+            }
+          />
+          <button type="submit">register</button>
+        </form>
+        <p>
+          Vous avez deja un compte
+          <button type="button" onClick={toggleRegister}>
+            Login
+          </button>
+        </p>
+      </Show>
+      <p>{error()}</p>
+    </main>
+  )
+}
+
+export default Login
