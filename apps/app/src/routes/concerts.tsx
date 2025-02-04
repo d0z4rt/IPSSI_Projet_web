4837a637e7f106c89b82befc7bf680aec7373d4c
Reicero <clarisse.daudel@gmail.com>
2025-02-04 18:15:03 +0100

    migrate

---
+++ apps/app/src/routes/concerts.tsx
@@ -0,0 +1,89 @@
+import { For, createSignal } from 'solid-js'
+import { concerts } from '../data/concert'
+import styles from './concerts.module.css'
+
+const Concerts = () => {
+  const [searchValue, setSearchValue] = createSignal('')
+  const [selectedDate, setSelectedDate] = createSignal('')
+  const [selectedGenre, setSelectedGenre] = createSignal('')
+
+  const filteredConcerts = () =>
+    concerts.filter((concert) => {
+      const nameMatch = concert.name.toLowerCase().includes(searchValue())
+
+      // Extraction de la date (mois année)
+      const concertDateMatch =
+        concert.date.toLowerCase().includes(selectedDate()) ||
+        selectedDate() === ''
+
+      // Vérification du genre
+      const concertGenres = concert.tag.map((genre) => genre.toLowerCase())
+      const genreMatch =
+        concertGenres.includes(selectedGenre()) || selectedGenre() === ''
+
+      return nameMatch && concertDateMatch && genreMatch
+    })
+
+  return (
+    <main class={styles['main-content']}>
+      <div class={styles['filter-bar']}>
+        <input
+          type="text"
+          id={styles.search}
+          onInput={(e) => setSearchValue(e.currentTarget.value.toLowerCase())}
+          placeholder="🔍 Rechercher un concert..."
+        />
+
+        <select
+          onChange={(e) => setSelectedDate(e.currentTarget.value.toLowerCase())}
+        >
+          <option value="">📅 Date</option>
+          <option value="mai 2025">Mai 2025</option>
+          <option value="juin 2025">Juin 2025</option>
+          <option value="juillet 2025">Juillet 2025</option>
+        </select>
+
+        <select
+          id={styles['filter-genre']}
+          onChange={(e) =>
+            setSelectedGenre(e.currentTarget.value.toLowerCase())
+          }
+        >
+          <option value="">🎵 Genre</option>
+          <option value="rap">Rap</option>
+          <option value="pop">Pop</option>
+          <option value="punk">Punk</option>
+          <option value="metal">Metal</option>
+          <option value="electro">Electro</option>
+          <option value="reggae">Reggae</option>
+        </select>
+      </div>
+
+      <section class={styles['concerts-grid']} id="concerts-list">
+        <For each={filteredConcerts()}>
+          {(concert) => (
+            <div class={styles['concert-card']}>
+              <img
+                src={concert.img}
+                alt={concert.name}
+                class={styles['concert-img']}
+              />
+              <div class={styles['concert-info']}>
+                <h3>{concert.name}</h3>
+                <p>{concert.info}</p>
+                <p>
+                  <strong>{concert.date}</strong>
+                </p>
+                <button type="button" class={styles['btn-buy']}>
+                  Acheter des billets
+                </button>
+              </div>
+            </div>
+          )}
+        </For>
+      </section>
+    </main>
+  )
+}
+
+export default Concerts
---
+++ apps/app/src/routes/mentions.tsx
@@ -0,0 +1,34 @@
+const Mentions = () => {
+  return (
+    <main class="main-content">
+      <section class="bio">
+        <h2>Informations Éditeur</h2>
+        <p>
+          <strong>Nom de l'entreprise :</strong> Le Carré d'As
+        </p>
+        <p>
+          <strong>Adresse :</strong> 123 Rue Imaginaire, 34000 Montpellier,
+          France
+        </p>
+        <p>
+          <strong>Email :</strong> contact@carredas.fr
+        </p>
+
+        <h2>Hébergement</h2>
+        <p>
+          <strong>Hébergeur :</strong> Ce site est un projet fictif réalisé dans
+          le cadre d’un exercice scolaire à l'IPSSI Montpellier.
+        </p>
+        <p>Aucun hébergement commercial n'est utilisé.</p>
+
+        <h2>Propriété intellectuelle</h2>
+        <p>
+          Tout le contenu de ce site (textes, logo) est la propriété de Le Carré
+          d'As, sauf indication contraire.
+        </p>
+      </section>
+    </main>
+  )
+}
+
+export default Mentions
