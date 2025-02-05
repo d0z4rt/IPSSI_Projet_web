82019c75f7ef31fa447db7a6cd6f71f72081d157
Ricoland0 <yanisranque@gmail.com>
2025-02-05 14:11:54 +0100

    mtn mise en page

--- apps/app/src/data/jeu.ts
+++ apps/app/src/data/jeu.ts
@@ -1,170 +1,191 @@
 export const jeux = [
 { 
-    "name": "Uno",
-    "info": "Un jeu de cartes rapide et amusant où le but est de se débarrasser de toutes ses cartes en utilisant des couleurs et des chiffres correspondants.",
-    "img": "./images/uno.png",
-    "tag": ["Cartes"],
-    "time": "30"
+    name: "Uno",
+    info: "Un jeu de cartes rapide et amusant où le but est de se débarrasser de toutes ses cartes en utilisant des couleurs et des chiffres correspondants.",
+    img: "./images/uno.png",
+    tag: ["Cartes", "Famille"],
+    time: "30"
 },
 { 
-    "name": "Poker",
-    "info": "Un jeu de cartes stratégique et psychologique où les joueurs misent et bluffent pour obtenir la meilleure combinaison possible.",
-    "img": "./images/poker.png",
-    "tag": ["Cartes"],
-    "time": "45"
+    name: "Poker",
+    info: "Un jeu de cartes stratégique et psychologique où les joueurs misent et bluffent pour obtenir la meilleure combinaison possible.",
+    img: "./images/poker.png",
+    tag: ["Cartes", "Stratégie"],
+    time: "45"
 },
 { 
-    "name": "Tarot",
-    "info": "Un jeu de cartes classique français où un joueur affronte les autres en tant que preneur en tentant de remporter des plis stratégiques.",
-    "img": "./images/tarot.png",
-    "tag": ["Cartes"],
-    "time": "60"
+    name: "Tarot",
+    info: "Un jeu de cartes classique français où un joueur affronte les autres en tant que preneur en tentant de remporter des plis stratégiques.",
+    img: "./images/tarot.png",
+    tag: ["Cartes", "Classique"],
+    time: "60"
 },
 { 
-    "name": "Loup-Garou",
-    "info": "Un jeu de rôle impliquant des cartes où les joueurs incarnent soit des villageois, soit des loups-garous qui doivent éliminer les autres en bluffant et en discutant.",
-    "img": "./images/loup-garou.png",
-    "tag": ["Rôle"],
-    "time": "30"
+    name: "Loup-Garou",
+    info: "Un jeu de rôle impliquant des cartes où les joueurs incarnent soit des villageois, soit des loups-garous qui doivent éliminer les autres en bluffant et en discutant.",
+    img: "./images/loup-garou.png",
+    tag: ["Société", "Rôle"],
+    time: "30"
 },
 { 
-    "name": "Échecs",
-    "info": "Un jeu de réflexion et de stratégie où deux joueurs s'affrontent en déplaçant leurs pièces sur un échiquier pour mettre le roi adverse en échec et mat.",
-    "img": "./images/echecs.png",
-    "tag": ["Réflexion"],
-    "time": "90"
+    name: "Échecs",
+    info: "Un jeu de réflexion et de stratégie où deux joueurs s'affrontent en déplaçant leurs pièces sur un échiquier pour mettre le roi adverse en échec et mat.",
+    img: "./images/echecs.png",
+    tag: ["Réflexion", "Stratégie"],
+    time: "90"
 },
 { 
-    "name": "Dames",
-    "info": "Un jeu de réflexion où les joueurs déplacent leurs pions en diagonale pour capturer ceux de l'adversaire et atteindre le dernier rang du plateau.",
-    "img": "./images/dames.png",
-    "tag": ["Réflexion"],
-    "time": "45"
+    name: "Dames",
+    info: "Un jeu de réflexion où les joueurs déplacent leurs pions en diagonale pour capturer ceux de l'adversaire et atteindre le dernier rang du plateau.",
+    img: "./images/dames.png",
+    tag: ["Réflexion", "Classique"],
+    time: "45"
 },
 { 
-    "name": "Dominos",
-    "info": "Un jeu de stratégie où les joueurs associent des tuiles portant des nombres identiques pour marquer des points et bloquer l’adversaire.",
-    "img": "./images/dominos.png",
-    "tag": ["Réflexion"],
-    "time": "30"
+    name: "Dominos",
+    info: "Un jeu de stratégie où les joueurs associent des tuiles portant des nombres identiques pour marquer des points et bloquer l’adversaire.",
+    img: "./images/dominos.png",
+    tag: ["Réflexion", "Famille"],
+    time: "30"
 },
 { 
-    "name": "Monopoly",
-    "info": "Un jeu de société où les joueurs achètent, vendent et échangent des propriétés pour devenir le plus riche et faire faillite les adversaires.",
-    "img": "./images/monopoly.png",
-    "tag": ["Stratégie"],
-    "time": "120"
+    name: "Monopoly",
+    info: "Un jeu de société où les joueurs achètent, vendent et échangent des propriétés pour devenir le plus riche et faire faillite les adversaires.",
+    img: "./images/monopoly.png",
+    tag: ["Société", "Stratégie"],
+    time: "120"
 },
 { 
-    "name": "Shuffleboard",
-    "info": "Un jeu d'adresse où les joueurs lancent des disques sur une surface pour marquer des points en atteignant certaines zones.",
-    "img": "./images/shuffleboard.png",
-    "tag": ["Adresse"],
-    "time": "60"
+    name: "Shuffleboard",
+    info: "Un jeu d'adresse où les joueurs lancent des disques sur une surface pour marquer des points en atteignant certaines zones.",
+    img: "./images/shuffleboard.png",
+    tag: ["Société", "Adresse"],
+    time: "60"
 },
 { 
-    "name": "Baby-Foot",
-    "info": "Un jeu de société compétitif où deux équipes contrôlent des figurines pour marquer des buts avec une petite balle.",
-    "img": "./images/baby-foot.png",
-    "tag": ["Société"],
-    "time": "10"
+    name: "Baby-Foot",
+    info: "Un jeu de société compétitif où deux équipes contrôlent des figurines pour marquer des buts avec une petite balle.",
+    img: "./images/baby-foot.png",
+    tag: ["Société", "Adresse"],
+    time: "10"
 },
 { 
-    "name": "Dobble",
-    "info": "Un jeu de rapidité et d'observation où les joueurs doivent trouver le symbole commun entre deux cartes le plus vite possible.",
-    "img": "./images/dobble.png",
-    "tag": ["Société"],
-    "time": "15"
+    name: "Dobble",
+    info: "Un jeu de rapidité et d'observation où les joueurs doivent trouver le symbole commun entre deux cartes le plus vite possible.",
+    img: "./images/dobble.png",
+    tag: ["Société", "Observation"],
+    time: "15"
 },
 { 
-    "name": "Time’s Up!",
-    "info": "Un jeu de société où les joueurs font deviner des personnalités, objets ou lieux à leur équipe en utilisant différents indices.",
-    "img": "./images/times-up.png",
-    "tag": ["Société"],
-    "time": "45"
+    name: "Time’s Up!",
+    info: "Un jeu de société où les joueurs font deviner des personnalités, objets ou lieux à leur équipe en utilisant différents indices.",
+    img: "./images/times-up.png",
+    tag: ["Société", "Famille"],
+    time: "45"
 },
 { 
-    "name": "Jenga",
-    "info": "Un jeu d'adresse où les joueurs retirent des blocs d'une tour en bois sans la faire tomber.",
-    "img": "./images/jenga.png",
-    "tag": ["Adresse"],
-    "time": "20"
+    name: "Jenga",
+    info: "Un jeu d'adresse où les joueurs retirent des blocs d'une tour en bois sans la faire tomber.",
+    img: "./images/jenga.png",
+    tag: ["Société", "Adresse"],
+    time: "20"
 },
 { 
-    "name": "Fléchettes",
-    "info": "Un jeu de précision où les joueurs lancent des fléchettes sur une cible pour marquer des points.",
-    "img": "./images/flechettes.png",
-    "tag": ["Adresse"],
-    "time": "30"
+    name: "Fléchettes",
+    info: "Un jeu de précision où les joueurs lancent des fléchettes sur une cible pour marquer des points.",
+    img: "./images/flechettes.png",
+    tag: ["Société", "Adresse"],
+    time: "30"
 },
 { 
-    "name": "Machine à poing",
-    "info": "Un jeu d'arcade où les joueurs testent leur force en frappant un sac pour obtenir le meilleur score.",
-    "img": "./images/machine-a-poing.png",
-    "tag": ["Arcade"],
-    "time": "5"
+    name: "Machine à poing",
+    info: "Un jeu d'arcade où les joueurs testent leur force en frappant un sac pour obtenir le meilleur score.",
+    img: "./images/machine-a-poing.png",
+    tag: ["Société", "Arcade"],
+    time: "5"
 },
 { 
-    "name": "Puissance 4",
-    "info": "Un jeu de réflexion où les joueurs doivent aligner quatre jetons de leur couleur pour gagner.",
-    "img": "./images/puissance4.png",
-    "tag": ["Réflexion"],
-    "time": "15"
+    name: "Puissance 4",
+    info: "Un jeu de réflexion où les joueurs doivent aligner quatre jetons de leur couleur pour gagner.",
+    img: "./images/puissance4.png",
+    tag: ["Réflexion", "Famille"],
+    time: "15"
 },
 { 
-    "name": "Risk",
-    "info": "Un jeu de stratégie où les joueurs conquièrent des territoires en utilisant des dés et des armées.",
-    "img": "./images/risk.png",
-    "tag": ["Stratégie"],
-    "time": "180"
+    name: "Risk",
+    info: "Un jeu de stratégie où les joueurs conquièrent des territoires en utilisant des dés et des armées.",
+    img: "./images/risk.png",
+    tag: ["Société", "Stratégie"],
+    time: "180"
 },
 { 
-    "name": "Scrabble",
-    "info": "Un jeu de lettres où les joueurs forment des mots sur un plateau pour marquer des points.",
-    "img": "./images/scrabble.png",
-    "tag": ["Réflexion"],
-    "time": "60"
+    name: "Scrabble",
+    info: "Un jeu de lettres où les joueurs forment des mots sur un plateau pour marquer des points.",
+    img: "./images/scrabble.png",
+    tag: ["Réflexion", "Lettres"],
+    time: "60"
 },
 { 
-    "name": "Cluedo",
-    "info": "Un jeu d'enquête où les joueurs doivent résoudre un meurtre en trouvant l'arme, le lieu et le coupable.",
-    "img": "./images/cluedo.png",
-    "tag": ["Stratégie"],
-    "time": "60"
+    name: "Cluedo",
+    info: "Un jeu d'enquête où les joueurs doivent résoudre un meurtre en trouvant l'arme, le lieu et le coupable.",
+    img: "./images/cluedo.png",
+    tag: ["Société", "Enquête"],
+    time: "60"
 },
 { 
-    "name": "Mikado",
-    "info": "Un jeu d'adresse où les joueurs doivent retirer des bâtonnets d'un tas sans faire bouger les autres.",
-    "img": "./images/mikado.png",
-    "tag": ["Adresse"],
-    "time": "15"
+    name: "Mikado",
+    info: "Un jeu d'adresse où les joueurs doivent retirer des bâtonnets d'un tas sans faire bouger les autres.",
+    img: "./images/mikado.png",
+    tag: ["Société", "Adresse"],
+    time: "15"
 },
 { 
-    "name": "Pictionary",
-    "info": "Un jeu de dessin où les joueurs doivent faire deviner un mot ou une phrase à leur équipe en dessinant.",
-    "img": "./images/pictionary.png",
-    "tag": ["Société"],
-    "time": "30"
+    name: "Pictionary",
+    info: "Un jeu de dessin où les joueurs doivent faire deviner un mot ou une phrase à leur équipe en dessinant.",
+    img: "./images/pictionary.png",
+    tag: ["Société", "Famille"],
+    time: "30"
 },
 { 
-    "name": "Twister",
-    "info": "Un jeu physique où les joueurs doivent placer leurs mains et pieds sur des cercles de couleur sans tomber.",
-    "img": "./images/twister.png",
-    "tag": ["Société"],
-    "time": "20"
+    name: "Twister",
+    info: "Un jeu physique où les joueurs doivent placer leurs mains et pieds sur des cercles de couleur sans tomber.",
+    img: "./images/twister.png",
+    tag: ["Société", "Physique"],
+    time: "20"
 },
 { 
-    "name": "Trivial Pursuit",
-    "info": "Un jeu de culture générale où les joueurs répondent à des questions dans différentes catégories pour remplir leur camembert.",
-    "img": "./images/trivial-pursuit.png",
-    "tag": ["Réflexion"],
-    "time": "90"
+    name: "Trivial Pursuit",
+    info: "Un jeu de culture générale où les joueurs répondent à des questions dans différentes catégories pour remplir leur camembert.",
+    img: "./images/trivial-pursuit.png",
+    tag: ["Réflexion", "Culture générale"],
+    time: "90"
 },
 { 
-    "name": "Beer Pong",
-    "info": "Un jeu d'adresse où les joueurs lancent une balle de ping-pong dans des gobelets remplis de boisson.",
-    "img": "./images/beer-pong.png",
-    "tag": ["Adresse"],
-    "time": "20"
+    name: "Beer Pong",
+    info: "Un jeu d'adresse où les joueurs lancent une balle de ping-pong dans des gobelets remplis de boisson.",
+    img: "./images/beer-pong.png",
+    tag: ["Société", "Fête"],
+    time: "20"
+},
+{ 
+    name: "Catan",
+    info: "Un jeu de stratégie où les joueurs collectent et échangent des ressources pour construire des colonies et des routes.",
+    img: "./images/catan.png",
+    tag: ["Société", "Ressources"],
+    time: "90"
+},
+{ 
+    name: "Pandemic",
+    info: "Un jeu coopératif où les joueurs travaillent ensemble pour arrêter la propagation de maladies à travers le monde.",
+    img: "./images/pandemic.png",
+    tag: ["Société", "Coopératif"],
+    time: "60"
+},
+{ 
+    name: "BlackJack",
+    info: "Un jeu de cartes populaire où les joueurs tentent d'obtenir une main dont la valeur est la plus proche possible de 21 sans la dépasser.",
+    img: "./images/blackjack.png",
+    tag: ["Cartes", "Casino"],
+    time: "30"
 }
 ]
--- apps/app/src/routes/jeux.tsx
+++ apps/app/src/routes/jeux.tsx
@@ -1,104 +1,144 @@
-import {For} from 'solid-js';
+import {For, createSignal, Show} from 'solid-js';
 import {jeux} from '../data/jeu';
 import styles from './jeu.module.css';
+import Card from '../components/Card'
 
 
 const Jeu = () => {
-
-
+  const [searchValue, setSearchValue] = createSignal('');
+  const [selectedGenre, setSelectedGenre] = createSignal('');
+  const [sortCriteria, setSortCriteria] = createSignal('name');
+  const [showFilterPage, setShowFilterPage] = createSignal(false);
+  
+  const allTags = Array.from(new Set(jeux.flatMap(jeu => jeu.tag.map(tag => tag.toLowerCase()))));
+  
+  const filteredJeux = () =>
+  jeux.filter((jeu) => {
+    const nameMatch = jeu.name.toLowerCase().includes(searchValue());
+    const genreMatch = selectedGenre() === '' || jeu.tag.map((genre) => genre.toLowerCase()).includes(selectedGenre());
+    
+    return nameMatch && genreMatch;
+  });
+  const sortedJeux = () => {
+    const jeuxToSort = filteredJeux();
+    return jeuxToSort.sort((a, b) => {
+      if (sortCriteria() === 'name') {
+        return a.name.localeCompare(b.name);
+      } else if (sortCriteria() === 'time') {
+        return parseInt(a.time) - parseInt(b.time);
+      }
+      return 0;
+    });
+  };
+  
+  
   return (
   <>
+  <div class={styles['filter-bar']}>
+    <div class={styles['filter-bar']}>
+      <input
+      type="text"
+      id={styles.search}
+      onInput={(e) => setSearchValue(e.currentTarget.value.toLowerCase())}
+      placeholder="🔍 Rechercher un jeu..."
+      />
+    </div>
+    <div class={styles['filter-bar']}>
+      <button class={styles.btn_list} type="button" onClick={() => setShowFilterPage(true)}>Filtres</button>
+    </div>
+    <select id="sort" aria-label="sort"class={styles['filter-genre']} onChange={(e) => setSortCriteria(e.currentTarget.value)}>
+      <option value="default" disabled selected hidden>Trier par</option>
+      <option value="name">Nom</option>
+      <option value="time">Temps</option>
+    </select>
+  </div>
   
-  <div>
-    <div class={styles.title}>
-       <h1>Jeux</h1>
+  
+  <Show when={showFilterPage()}>
+    <div class={styles['filter-page']}>
+      <button class={styles['close-btn']} onClick={() => setShowFilterPage(false)} type='button'>Fermer</button>
+      <button id="all" class={styles.btn_list} type="button" onClick={() => {
+        setSelectedGenre('');
+        setShowFilterPage(false);
+      }}>Tout</button>
+      <For each={allTags}>
+        {(tag) => (
+          <button
+          class={styles.btn_list}
+          type="button"
+          onClick={() => {
+            setSelectedGenre(tag);
+            setShowFilterPage(false);
+          }}
+          >
+          {tag.charAt(0).toUpperCase() + tag.slice(1)}
+        </button>
+        )}
+      </For>
     </div>
-    <div class={styles.select}>
-      <div id="search">
-        <label for="search">Rechercher un jeu</label>
-        <input type="text" id="search" name="search" placeholder="Rechercher un jeu"/>
-      </div>
-      <div id="filter">
-        <div id="btnlist">
-          <button id="all" class={'styles.btn-list'} type="button">Tout</button>
-          <button id="card" class={'styles.btn-list'} type="button">Société</button>
-          <button id="reflex" class={'styles.btn-list'} type="button">Réflexion</button>
-          <button id="arc" class={'styles.btn-list'} type="button">Arcade</button>
-          <button id="role" class={'styles.btn-list'} type="button">Rôle</button>
-          <button id="adrr" class={'styles.btn-list'} type="button">Adresse</button>
-        </div>
-      </div>
-      <div id="sort">
-        <select name="sorter" id="sortlist" title="Trier par">
-          <option value="name">Nom</option>
-          <option value="type">Type</option>
-          <option value="age">Age</option>
-          <option value="time">Durée</option>
-        </select>
-      </div>
+  </Show>
+  
+  
+  <div class={styles.cards} id="cards">
+    <For each={sortedJeux()}>
+      {(jeu) =>(
+        <Card title={jeu.name} cover={jeu.img} alt={jeu.name}>
+          <p>{jeu.info}</p>
+          <p>{jeu.time} minutes</p>
+          <p>{jeu.tag.join(', ')}</p>
+        </Card>
+        )}
+      </For>
     </div>
-    <div class={styles.cards} id="cards">
-      <For each={jeux}>{(jeu) => <>
-        <img src={jeu.img} alt={jeu.name}/>
-        <h3>{jeu.name}</h3>
-        <p>{jeu.info}</p>
-        <p>{jeu.time} minutes</p>
-        <p>{jeu.tag}</p>
-        </>
-      }</For>
-    </div>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     <section class={styles.contact}>
       <h2>Nous contacter</h2>
-      <form class={'styles.contact-form'}>
-        <div class={'styles.contact-name'}>
-          <div class={'styles.contact-info'}>
+      <form class={styles.contact_form}>
+        <div class={styles.contact_name}>
+          <div class={styles.contact_info}>
             <label for="name">Nom</label>
-            <input id="name" class={'styles.contact-text'} type="text" required/>
+            <input id="name" class={styles.contact_text} type="text" required/>
           </div>
-          <div class={'styles.contact-info'}>
+          <div class={styles.contact_info}>
             <label for="firstname">Prénom</label>
-            <input id="firstname" class={'styles.contact-text'} type="text" required/>
+            <input id="firstname" class={styles.contact_text} type="text" required/>
           </div>
         </div>
-        <div classList={{styles['contact-info']:true, [styles.othe]:true}}>
+        <div classList={{[styles.contact_info]:true, [styles.other]:true}}>
           <label for="email">Email</label>
-          <input id="email" class={"styles.contact-text"} type="email" required/>
+          <input id="email" class={styles.contact_text} type="email" required/>
         </div>
-        <div class={"styles.contact-info other"}>
+        <div classList={{[styles.contact_info]:true, [styles.other]:true}}>
           <label for="subject">Sujet</label>
-          <input id="subject" class={"styles.contact-text"} type="text" required/>
+          <input id="subject" class={styles.contact_text} type="text" required/>
         </div>
-        <div class="styles.contactinfo other">
+        <div classList={{[styles.contact_info]:true,  [styles.other]:true}}>
           <label for="message">Message</label>
-          <input id="message" class="styles.contacttext" type="text" required/>
+          <input id="message" class={styles.contact_text} type="text" required/>
         </div>
-        <button class="styles.button" type="submit">Envoyer</button>
+        <button class={styles.button} type="submit">Envoyer</button>
       </form>
     </section> 
     
-  </div>
-  <footer class="styles.footer">
-    <div class="styles.footer-content">
-      <p>&#x1f4cd; 123 Rue des Stratèges, Montpellier</p>
-      <p>&#x1f4de; 04 12 34 56 78 | ✉️ contact@carredas.fr</p>
-      <div class="'styles.footerho'urs">
-        <p>&#x1f552; Lundi - Jeudi : 18h - 00h | Vendredi - Dimanche : 17h - 02h</p>
-      </div>
-      <div class="styles.footer-social">
-        <a href="#"><img src="/images/facebook-icon.png" alt="Facebook"/></a>
-        <a href="#"><img src="/images/instagram-icon.png" alt="Instagram"/></a>
-        <a href="#"><img src="/images/twitter-icon.png" alt="Twitter"/></a>
-      </div>
-      <div class="styles.footer-legal">
-        <a href="#">Mentions légales</a> | 
-        <a href="#">Politique de confidentialité</a>
-      </div>
-      <p>© 2025 Le Carré d'As - Tous droits réservés</p>
-    </div>
-  </footer>
-  </>
-  );
-}
-
-export default Jeu;
+    
+    </>
+    );
+  }
+  
+  export default Jeu;
