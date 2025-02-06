cb5c06703fc6adefcbdae29b8cabce22ef31b544
Ricoland0 <yanisranque@gmail.com>
2025-02-06 19:28:24 +0100

    a

--- apps/app/src/routes/jeux.tsx
+++ apps/app/src/routes/jeux.tsx
@@ -5,15 +5,19 @@ import Card from '../components/Card'
 import FormInput from '../components/FormInput'
 import Button from '../components/Button';
 
+// Composant principal pour afficher les jeux
 const Jeu = () => {
+  // Déclaration des signaux pour la recherche, les tags et les critères de tri
   const [searchValue, setSearchValue] = createSignal('');
   const [selectedMainTag, setSelectedMainTag] = createSignal('');
   const [selectedSubTag, setSelectedSubTag] = createSignal('');
   const [sortCriteria, setSortCriteria] = createSignal('name');
   const [showFilterPage, setShowFilterPage] = createSignal(false);
   
+  // Récupération de tous les tags principaux uniques
   const allMainTags = Array.from(new Set(jeux.map(jeu => jeu.mainTag.toLowerCase())));
   
+  // Calcul des tags secondaires disponibles en fonction du tag principal sélectionné
   const availableSubTags = createMemo(() => {
     if (selectedMainTag() === '') {
       return Array.from(new Set(jeux.map(jeu => jeu.subTag.toLowerCase())));
@@ -21,15 +25,17 @@ const Jeu = () => {
     return Array.from(new Set(jeux.filter(jeu => jeu.mainTag.toLowerCase() === selectedMainTag()).map(jeu => jeu.subTag.toLowerCase())));
   });
   
+  // Filtrage des jeux en fonction des critères de recherche et des tags sélectionnés
   const filteredJeux = () =>
-  jeux.filter((jeu) => {
-    const nameMatch = jeu.name.toLowerCase().includes(searchValue());
-    const mainTagMatch = selectedMainTag() === '' || jeu.mainTag.toLowerCase() === selectedMainTag();
-    const subTagMatch = selectedSubTag() === '' || jeu.subTag.toLowerCase() === selectedSubTag();
-    
-    return nameMatch && mainTagMatch && subTagMatch;
-  });
-  
+    jeux.filter((jeu) => {
+      const nameMatch = jeu.name.toLowerCase().includes(searchValue());
+      const mainTagMatch = selectedMainTag() === '' || jeu.mainTag.toLowerCase() === selectedMainTag();
+      const subTagMatch = selectedSubTag() === '' || jeu.subTag.toLowerCase() === selectedSubTag();
+      
+      return nameMatch && mainTagMatch && subTagMatch;
+    });
+
+  // Tri des jeux filtrés en fonction des critères de tri
   const sortedJeux = () => {
     const jeuxToSort = filteredJeux();
     return jeuxToSort.sort((a, b) => {
@@ -44,88 +50,92 @@ const Jeu = () => {
   };
   
   return (
-  <>
-  
-  <div class={styles['filter-bar']}>
-    
-    <FormInput 
-      type="text"
-      id={styles.search}
-      onInput={(e)=> setSearchValue(e.currentTarget.value.toLowerCase())}
-      placeholder="🔍 Rechercher un jeu..."
-    />
+    <>
+      {/* Barre de filtre */}
+      <div class={styles['filter-bar']}>
+        <FormInput 
+          type="text"
+          id={styles.search}
+          onInput={(e)=> setSearchValue(e.currentTarget.value.toLowerCase())}
+          placeholder="🔍 Rechercher un jeu..."
+        />
 
-    <Button class={styles.filter_btn} type="button" onClick={() => setShowFilterPage(true)}>Filtres</Button>
+        <Button class={styles.filter_btn} type="button" onClick={() => setShowFilterPage(true)}>Filtres</Button>
+        
+        <select id="sort" aria-label="sort" class={styles.filter_select} onChange={(e) => setSortCriteria(e.currentTarget.value)}>
+          <option value="default" disabled selected hidden>Trier par</option>
+          <option value="name">Nom</option>
+          <option value="time">Temps</option>
+        </select>
+      </div>
       
+      {/* Page de filtre */}
+      <Show when={showFilterPage()}>
+        <div class={styles['filter-page']}>
+          {/* Bouton pour fermer la page de filtre */}
+          <button class={styles['close-btn']} onClick={() => setShowFilterPage(false)} type='button'>Fermer</button>
+          
+          {/* Bouton pour réinitialiser les filtres */}
+          <button id="all" class={styles.btn_list} type="button" onClick={() => {
+            setSelectedMainTag('');
+            setSelectedSubTag('');
+            setShowFilterPage(false);
+          }}>Tout</button>
+          
+          {/* Section pour sélectionner le genre principal */}
+          <h3>Genre principal</h3>
+          <div class={styles['tag-container']}>
+            <For each={allMainTags}>
+              {(tag) => (
+                <button
+                  class={`${styles.btn_list} ${styles.mainTag} ${styles[tag.toLowerCase().replace(/\s+/g, '-')]} ${selectedMainTag() === tag ? styles.selected : ''}`}
+                  type="button"
+                  onClick={() => {
+                    setSelectedMainTag(selectedMainTag() === tag ? '' : tag);
+                    setSelectedSubTag('');
+                  }}
+                >
+                  {tag.charAt(0).toUpperCase() + tag.slice(1)}
+                </button>
+              )}
+            </For>
+          </div>
+          
+          {/* Section pour sélectionner le genre secondaire */}
+          <h3>Genre secondaire</h3>
+          <div class={styles['tag-container']}>
+            <For each={availableSubTags()}>
+              {(tag) => (
+                <button
+                  class={`${styles.btn_list} ${styles.subTag} ${selectedSubTag() === tag ? styles.selected : ''}`}
+                  type="button"
+                  onClick={() => {
+                    setSelectedSubTag(selectedSubTag() === tag ? '' : tag);
+                  }}
+                >
+                  {tag.charAt(0).toUpperCase() + tag.slice(1)}
+                </button>
+              )}
+            </For>
+          </div>
+        </div>
+      </Show>
       
-      <select id="sort" aria-label="sort" class={styles.filter_select} onChange={(e) => setSortCriteria(e.currentTarget.value)}>
-        <option value="default" disabled selected hidden>Trier par</option>
-        <option value="name">Nom</option>
-        <option value="time">Temps</option>
-      </select>
-    
-  </div>
-  
-
-
-  <Show when={showFilterPage()}>
-    <div class={styles['filter-page']}>
-      <button class={styles['close-btn']} onClick={() => setShowFilterPage(false)} type='button'>Fermer</button>
-      <button id="all" class={styles.btn_list} type="button" onClick={() => {
-        setSelectedMainTag('');
-        setSelectedSubTag('');
-        setShowFilterPage(false);
-      }}>Tout</button>
-      <h3>Genre principal</h3>
-      <div class={styles['tag-container']}>
-        <For each={allMainTags}>
-          {(tag) => (
-            <button
-            class={`${styles.btn_list} ${styles.mainTag} ${styles[tag.toLowerCase().replace(/\s+/g, '-')]} ${selectedMainTag() === tag ? styles.selected : ''}`}
-            type="button"
-            onClick={() => {
-              setSelectedMainTag(selectedMainTag() === tag ? '' : tag);
-              setSelectedSubTag('');
-            }}
-            >
-            {tag.charAt(0).toUpperCase() + tag.slice(1)}
-          </button>
+      {/* Affichage des cartes de jeux */}
+      <div class={styles.cards} id="cards">
+        <For each={sortedJeux()}>
+          {(jeu) => (
+            <Card title={jeu.name} alt={jeu.name} type={jeu.mainTag}>
+              <img src={jeu.img} alt={jeu.name} class={styles.imgcard} />
+              <p>{jeu.info}</p>
+              <p>⏲{jeu.time} minutes</p>
+              <p class={styles.subTag}>{jeu.subTag}</p>
+            </Card>
           )}
         </For>
       </div>
-      <h3>Genre secondaire</h3>
-      <div class={styles['tag-container']}>
-        <For each={availableSubTags()}>
-          {(tag) => (
-            <button
-            class={`${styles.btn_list} ${styles.subTag} ${selectedSubTag() === tag ? styles.selected : ''}`}
-            type="button"
-            onClick={() => {
-              setSelectedSubTag(selectedSubTag() === tag ? '' : tag);
-            }}
-            >
-            {tag.charAt(0).toUpperCase() + tag.slice(1)}
-          </button>
-          )}
-        </For>
-      </div>
-    </div>
-  </Show>
-  
-  <div class={styles.cards} id="cards">
-    <For each={sortedJeux()}>
-      {(jeu) => (
-        <Card title={jeu.name} alt={jeu.name} type={jeu.mainTag}>
-          <img src={jeu.img} alt={jeu.name} class={styles.imgcard} />
-          <p>{jeu.info}</p>
-          <p>⏲{jeu.time} minutes</p>
-          <p class={styles.subTag}>{jeu.subTag}</p>
-        </Card>
-        )}
-      </For>
-    </div>
     </>
-    );
-  }
-  
-  export default Jeu;
+  );
+}
+
+export default Jeu;
