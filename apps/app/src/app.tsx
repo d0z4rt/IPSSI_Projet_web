f5a2a5d10404b891f0d0b0ae1803fc64c0a0ed42
d0z4rt <d0z@d0z.eu>
2025-02-07 03:33:26 +0100

    feat: add font, fix responsive, improve SEO

--- apps/app/src/app.tsx
+++ apps/app/src/app.tsx
@@ -1,4 +1,4 @@
-import { MetaProvider, Title } from '@solidjs/meta'
+import { Link, Meta, MetaProvider, Title } from '@solidjs/meta'
 import { Router } from '@solidjs/router'
 import { FileRoutes } from '@solidjs/start/router'
 import { Suspense } from 'solid-js'
@@ -14,7 +14,42 @@ export default function App() {
       root={(props) => (
         <MetaProvider>
           <AuthProvider>
-            <Title>SolidStart - Basic</Title>
+            <Title>Le Carré d'As</Title>
+            <Meta
+              name="description"
+              content={
+                'Un spot unique où jeux de société et concerts live se rencontrent dans une ambiance électrique. Que tu sois là pour défier tes potes autour d’un plateau ou te laisser porter par des vibes musicales endiablées, chaque soirée ici est une nouvelle expérience. Entre décor stylé, énergie conviviale et moments inoubliables, c’est l’endroit parfait pour chiller, jouer et vibrer.'
+              }
+            />
+            <Meta
+              name="twitter:card"
+              content="summary_large_image"
+              data-sm=""
+            />
+            <Meta name="twitter:site" content="@lecarredas" data-sm="" />
+            <Meta name="twitter:title" content="Le Carré d'As" data-sm="" />
+            <Meta name="twitter:image:src" content=" !TODO" data-sm="" />
+            <Meta
+              name="twitter:description"
+              content="Un spot unique où jeux de société et concerts live se rencontrent dans une ambiance électrique. Que tu sois là pour défier tes potes autour d’un plateau ou te laisser porter par des vibes musicales endiablées, chaque soirée ici est une nouvelle expérience. Entre décor stylé, énergie conviviale et moments inoubliables, c’est l’endroit parfait pour chiller, jouer et vibrer."
+              data-sm=""
+            />
+
+            <Meta property="og:type" content="website" data-sm="" />
+            <Meta property="og:site_name" content="Le Carré d'As" data-sm="" />
+            <Meta property="og:title" content="Le Carré d'As" data-sm="" />
+            <Meta
+              property="og:description"
+              content="Un spot unique où jeux de société et concerts live se rencontrent dans une ambiance électrique. Que tu sois là pour défier tes potes autour d’un plateau ou te laisser porter par des vibes musicales endiablées, chaque soirée ici est une nouvelle expérience. Entre décor stylé, énergie conviviale et moments inoubliables, c’est l’endroit parfait pour chiller, jouer et vibrer."
+              data-sm=""
+            />
+            <Meta property="og:image" content=" !TODO" data-sm="" />
+            <Meta property="og:image:alt" content="Le Carré d'As" data-sm="" />
+            <Meta property="og:image:width" content="1200" data-sm="" />
+            <Meta property="og:image:height" content="600" data-sm="" />
+            <Meta property="og:url" content="/" data-sm="" />
+
+            <Link rel="canonical" href={'/'} data-sm="" />
             <Header />
             <Suspense>{props.children}</Suspense>
             <Cookies />
